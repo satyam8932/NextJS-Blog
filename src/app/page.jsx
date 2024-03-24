17eb36b7e21df6aa38182b91630c912dfeec9823
Satyam Singh <satyamsingh5august@gmail.com>
2024-03-24 16:11:12 +0530

    starting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div></div>;
 }
